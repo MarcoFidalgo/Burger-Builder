55a5f3e9144e1eeaa2afb4d5859c3536db080d02
Marco <m_s_f_@hotmail.com>
2021-03-07 23:24:36 +0000

    Added Toolbar
    Toolbar
    - Added simple toolbar for desktop
    - Added centered logo
    Next step: Sidedrawer for mobile

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <Aux>
         <div>
-            Toolbar, SideDrawer, Backdrop
+            <Toolbar />
         </div>
         <main className={classes.Content}>
             {props.children}
